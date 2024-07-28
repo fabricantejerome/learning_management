ae381b309f3659f970f0488394387702205afdb6
Jerome <fabricantejerome@gmail.com>
2024-07-28 12:24:15 +0800

    Add admin lesson page

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,9 @@ Rails.application.routes.draw do
   resources :checkouts, only: [:create]
 
   namespace :admin do
-    resources :courses
+    resources :courses do
+      resources :lessons
+    end
   end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
