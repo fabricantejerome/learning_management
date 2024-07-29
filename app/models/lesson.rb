7c139d10f1ce71a218c6c44ef7cbc6c325693160
Jerome <fabricantejerome@gmail.com>
2024-07-29 14:14:16 +0800

    Show the lessons inside the course

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,8 @@
 class Lesson < ApplicationRecord
-    has_one_attached :video
-    
+    has_one_attached :video do |attachable|
+        attachable.variant :thumb, resize_to_limit: [500, 500]
+    end
+
     belongs_to :course
     has_many :lesson_users, dependent: :destroy
 
