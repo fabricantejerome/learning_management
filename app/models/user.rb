384bcbdb617d7674368c1148d5f3cf9893142d5b
Jerome <fabricantejerome@gmail.com>
2024-08-03 10:19:14 +0800

    Add stripe webhook and remove confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable,
-            :recoverable, :rememberable, :validatable, :confirmable
+            :recoverable, :rememberable, :validatable
 
     has_many :lesson_users, dependent: :destroy
     has_many :course_users, dependent: :destroy
